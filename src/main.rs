23fff3bbe0baf69b41175a20715d8830f43dd77d
Joao Moreno <mail@joaomoreno.com>
2020-12-05 16:26:44 +0000

    2. Output an Image

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,27 @@
  *-------------------------------------------------------------------------------------------------------------*/
 
 fn main() {
-    let name = "VS Code Remote - Containers";
-    println!("Hello, {}!", name);
+
+	// Image
+
+	let image_width = 256;
+	let image_height = 256;
+
+	// Render
+
+	print!("P3\n{0} {1}\n255\n", image_width, image_height);
+
+	for j in (0..image_height).rev() {
+		for i in 0..image_width {
+			let r = (i as f64) / ((image_width - 1) as f64);
+			let g = (j as f64) / ((image_height - 1) as f64);
+			let b = 0.25;
+
+			let ir = (r * 255.999) as i32;
+			let ig = (g * 255.999) as i32;
+			let ib = (b * 255.999) as i32;
+
+			print!("{0} {1} {2}\n", ir, ig, ib);
+		}
+	}
 }
