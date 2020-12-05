55c1df88d6493919848ccc93936699fe2d1923f8
Joao Moreno <mail@joaomoreno.com>
2020-12-05 19:26:53 +0000

    vec3 struct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,60 @@
 use std::fs::File;
 use std::io::BufWriter;
+use std::ops::{AddAssign, DivAssign, Index, MulAssign, Neg};
 use std::path::Path;
+
+struct Vec3([f64; 3]);
+
+impl Vec3 {
+	fn new() -> Vec3 {
+		Vec3 { 0: [0.0, 0.0, 0.0] }
+	}
+	fn new_with(x: f64, y: f64, z: f64) -> Vec3 {
+		Vec3 { 0: [x, y, z] }
+	}
+	fn length(&self) -> f64 {
+		self.length_squared().sqrt()
+	}
+	fn length_squared(&self) -> f64 {
+		self[0].powi(2) + self[1].powi(2) + self[2].powi(2)
+	}
+}
+
+impl Neg for Vec3 {
+	type Output = Vec3;
+	fn neg(self) -> Self::Output {
+		Vec3::new_with(-self[0], -self[1], -self[2])
+	}
+}
+
+impl Index<usize> for Vec3 {
+	type Output = f64;
+
+	fn index(&self, index: usize) -> &Self::Output {
+		&self.0[index]
+	}
+}
+
+impl AddAssign for Vec3 {
+	fn add_assign(&mut self, other: Self) {
+		*self = Vec3::new_with(self[0] + other[0], self[1] + other[1], self[2] + other[2]);
+	}
+}
+
+impl MulAssign<f64> for Vec3 {
+	fn mul_assign(&mut self, t: f64) {
+		*self = Vec3::new_with(self[0] * t, self[1] * t, self[2] * t);
+	}
+}
+
+impl DivAssign<f64> for Vec3 {
+	fn div_assign(&mut self, t: f64) {
+		*self *= 1.0 / t;
+	}
+}
+
+type Point3 = Vec3;
+type Color = Vec3;
 
 fn main() {
 	// Image
