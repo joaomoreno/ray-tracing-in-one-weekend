14257aee3447c2a36600568d407e26a6a16f5686
Joao Moreno <mail@joaomoreno.com>
2020-12-05 18:54:43 +0000

    render pngs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
-/*--------------------------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
- *-------------------------------------------------------------------------------------------------------------*/
+use std::path::Path;
+use std::fs::File;
+use std::io::BufWriter;
+
+
 
 fn main() {
-
 	// Image
-
+	
 	let image_width = 256;
 	let image_height = 256;
 
 	// Render
 
-	print!("P3\n{0} {1}\n255\n", image_width, image_height);
+	let mut pixels = Vec::with_capacity(image_width * image_height);
 
 	for j in (0..image_height).rev() {
 		for i in 0..image_width {
@@ -20,11 +20,26 @@ fn main() {
 			let g = (j as f64) / ((image_height - 1) as f64);
 			let b = 0.25;
 
-			let ir = (r * 255.999) as i32;
-			let ig = (g * 255.999) as i32;
-			let ib = (b * 255.999) as i32;
+			let ir = (r * 255.999) as u8;
+			let ig = (g * 255.999) as u8;
+			let ib = (b * 255.999) as u8;
 
-			print!("{0} {1} {2}\n", ir, ig, ib);
+			pixels.push(ir);
+			pixels.push(ig);
+			pixels.push(ib);
 		}
 	}
+
+	// Write image
+
+	let path = Path::new(r"image.png");
+	let file = File::create(path).unwrap();
+	let ref mut w = BufWriter::new(file);
+
+	let mut encoder = png::Encoder::new(w, image_width as u32, image_height as u32);
+	encoder.set_color(png::ColorType::RGB);
+	encoder.set_depth(png::BitDepth::Eight);
+	let mut writer = encoder.write_header().unwrap();
+
+	writer.write_image_data(&pixels).unwrap();
 }
