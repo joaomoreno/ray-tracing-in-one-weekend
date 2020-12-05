19c5e3b79e416460fda0e7b8517c52de5c330f94
Joao Moreno <mail@joaomoreno.com>
2020-12-05 21:26:57 +0000

    3.2 vec3 Utility Functions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
 use std::io::BufWriter;
-use std::ops::{AddAssign, DivAssign, Index, MulAssign, Neg};
+use std::ops::{Add, AddAssign, Div, DivAssign, Index, Mul, MulAssign, Neg, Sub};
 use std::path::Path;
 
 struct Vec3([f64; 3]);
@@ -22,7 +22,7 @@ impl Vec3 {
 
 impl Neg for Vec3 {
 	type Output = Vec3;
-	fn neg(self) -> Self::Output {
+	fn neg(self) -> Vec3 {
 		Vec3::new_with(-self[0], -self[1], -self[2])
 	}
 }
@@ -30,7 +30,7 @@ impl Neg for Vec3 {
 impl Index<usize> for Vec3 {
 	type Output = f64;
 
-	fn index(&self, index: usize) -> &Self::Output {
+	fn index(&self, index: usize) -> &f64 {
 		&self.0[index]
 	}
 }
@@ -51,6 +51,99 @@ impl DivAssign<f64> for Vec3 {
 	fn div_assign(&mut self, t: f64) {
 		*self *= 1.0 / t;
 	}
+}
+
+trait Vec3Collection {
+	fn push_vec(&mut self, vec3: &Vec3);
+}
+
+impl Vec3Collection for Vec<u8> {
+	fn push_vec(&mut self, vec: &Vec3) {
+		self.push((vec[0] * 255.999) as u8);
+		self.push((vec[1] * 255.999) as u8);
+		self.push((vec[2] * 255.999) as u8);
+	}
+}
+
+impl Add for Vec3 {
+	type Output = Vec3;
+	fn add(self, other: Vec3) -> Vec3 {
+		Vec3::new_with(self[0] + other[0], self[1] + other[1], self[2] + other[2])
+	}
+}
+
+impl Sub for Vec3 {
+	type Output = Vec3;
+	fn sub(self, other: Vec3) -> Vec3 {
+		Vec3::new_with(self[0] - other[0], self[1] - other[1], self[2] - other[2])
+	}
+}
+
+impl Mul for Vec3 {
+	type Output = Vec3;
+	fn mul(self, other: Vec3) -> Vec3 {
+		Vec3::new_with(self[0] * other[0], self[1] * other[1], self[2] * other[2])
+	}
+}
+
+impl<T: Into<f64>> Mul<T> for Vec3 {
+	type Output = Vec3;
+	fn mul(self, t: T) -> Vec3 {
+		let t = t.into();
+		Vec3::new_with(self[0] * t, self[1] * t, self[2] * t)
+	}
+}
+
+impl<T: Into<f64>> Mul<T> for &Vec3 {
+	type Output = Vec3;
+	fn mul(self, t: T) -> Vec3 {
+		let t = t.into();
+		Vec3::new_with(self[0] * t, self[1] * t, self[2] * t)
+	}
+}
+
+impl Mul<Vec3> for f64 {
+	type Output = Vec3;
+	fn mul(self, vec: Vec3) -> Vec3 {
+		vec * self
+	}
+}
+
+impl Mul<Vec3> for u32 {
+	type Output = Vec3;
+	fn mul(self, vec: Vec3) -> Vec3 {
+		vec * self
+	}
+}
+
+impl<T: Into<f64>> Div<T> for Vec3 {
+	type Output = Vec3;
+	fn div(self, t: T) -> Vec3 {
+		self * (1.0 / t.into())
+	}
+}
+
+impl<T: Into<f64>> Div<T> for &Vec3 {
+	type Output = Vec3;
+	fn div(self, t: T) -> Vec3 {
+		self * (1.0 / t.into())
+	}
+}
+
+fn dot(u: &Vec3, v: &Vec3) -> f64 {
+	u[0] * v[0] + u[1] * v[1] + u[2] * v[2]
+}
+
+fn cross(u: &Vec3, v: &Vec3) -> Vec3 {
+	Vec3::new_with(
+		u[1] * v[2] - u[2] * v[1],
+		u[2] * v[0] - u[0] * v[2],
+		u[0] * v[1] - u[1] * v[0],
+	)
+}
+
+fn unit_vector(v: &Vec3) -> Vec3 {
+	v / v.length()
 }
 
 type Point3 = Vec3;
