9ddd13096baa1a7976e8883ca2b3540eff1d6c28
Joao Moreno <mail@joaomoreno.com>
2020-12-07 20:19:34 +0000

    cleaner ops overloading

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+#[macro_use]
+extern crate impl_ops;
+
 use std::fs::File;
 use std::io::BufWriter;
-use std::ops::{Add, AddAssign, Div, DivAssign, Index, Mul, MulAssign, Neg, Sub};
+use std::ops;
 use std::path::Path;
 
 #[derive(Clone)]
@@ -30,39 +33,6 @@ impl Vec3 {
 	}
 }
 
-impl Neg for Vec3 {
-	type Output = Vec3;
-	fn neg(self) -> Vec3 {
-		Vec3::new_with(-self[0], -self[1], -self[2])
-	}
-}
-
-impl Index<usize> for Vec3 {
-	type Output = f64;
-
-	fn index(&self, index: usize) -> &f64 {
-		&self.0[index]
-	}
-}
-
-impl AddAssign for Vec3 {
-	fn add_assign(&mut self, other: Self) {
-		*self = Vec3::new_with(self[0] + other[0], self[1] + other[1], self[2] + other[2]);
-	}
-}
-
-impl MulAssign<f64> for Vec3 {
-	fn mul_assign(&mut self, t: f64) {
-		*self = Vec3::new_with(self[0] * t, self[1] * t, self[2] * t);
-	}
-}
-
-impl DivAssign<f64> for Vec3 {
-	fn div_assign(&mut self, t: f64) {
-		*self *= 1.0 / t;
-	}
-}
-
 trait Vec3Collection {
 	fn push_vec(&mut self, vec3: &Vec3);
 }
@@ -75,119 +45,44 @@ impl Vec3Collection for Vec<u8> {
 	}
 }
 
-impl Add for Vec3 {
-	type Output = Vec3;
-	fn add(self, other: Vec3) -> Vec3 {
-		Vec3::new_with(self[0] + other[0], self[1] + other[1], self[2] + other[2])
+impl std::ops::Index<usize> for Vec3 {
+	type Output = f64;
+
+	fn index(&self, index: usize) -> &f64 {
+		&self.0[index]
 	}
 }
 
-impl Add for &Vec3 {
-	type Output = Vec3;
-	fn add(self, other: &Vec3) -> Vec3 {
-		Vec3::new_with(self[0] + other[0], self[1] + other[1], self[2] + other[2])
+impl std::ops::AddAssign for Vec3 {
+	fn add_assign(&mut self, other: Self) {
+		*self = Vec3::new_with(self[0] + other[0], self[1] + other[1], self[2] + other[2]);
 	}
 }
 
-impl Add<Vec3> for &Vec3 {
-	type Output = Vec3;
-	fn add(self, other: Vec3) -> Vec3 {
-		Vec3::new_with(self[0] + other[0], self[1] + other[1], self[2] + other[2])
+impl std::ops::MulAssign<f64> for Vec3 {
+	fn mul_assign(&mut self, t: f64) {
+		*self = Vec3::new_with(self[0] * t, self[1] * t, self[2] * t);
 	}
 }
 
-impl Add<&Vec3> for Vec3 {
-	type Output = Vec3;
-	fn add(self, other: &Vec3) -> Vec3 {
-		Vec3::new_with(self[0] + other[0], self[1] + other[1], self[2] + other[2])
+impl std::ops::DivAssign<f64> for Vec3 {
+	fn div_assign(&mut self, t: f64) {
+		*self *= 1.0 / t;
 	}
 }
 
-impl Sub for Vec3 {
-	type Output = Vec3;
-	fn sub(self, other: Vec3) -> Vec3 {
-		Vec3::new_with(self[0] - other[0], self[1] - other[1], self[2] - other[2])
-	}
-}
-
-impl Sub for &Vec3 {
-	type Output = Vec3;
-	fn sub(self, other: &Vec3) -> Vec3 {
-		Vec3::new_with(self[0] - other[0], self[1] - other[1], self[2] - other[2])
-	}
-}
-
-impl Sub<Vec3> for &Vec3 {
-	type Output = Vec3;
-	fn sub(self, other: Vec3) -> Vec3 {
-		Vec3::new_with(self[0] - other[0], self[1] - other[1], self[2] - other[2])
-	}
-}
-
-impl Sub<&Vec3> for Vec3 {
-	type Output = Vec3;
-	fn sub(self, other: &Vec3) -> Vec3 {
-		Vec3::new_with(self[0] - other[0], self[1] - other[1], self[2] - other[2])
-	}
-}
-
-impl Mul for Vec3 {
-	type Output = Vec3;
-	fn mul(self, other: Vec3) -> Vec3 {
-		Vec3::new_with(self[0] * other[0], self[1] * other[1], self[2] * other[2])
-	}
-}
-
-impl<T: Into<f64>> Mul<T> for Vec3 {
-	type Output = Vec3;
-	fn mul(self, t: T) -> Vec3 {
-		let t = t.into();
-		Vec3::new_with(self[0] * t, self[1] * t, self[2] * t)
-	}
-}
-
-impl<T: Into<f64>> Mul<T> for &Vec3 {
-	type Output = Vec3;
-	fn mul(self, t: T) -> Vec3 {
-		let t = t.into();
-		Vec3::new_with(self[0] * t, self[1] * t, self[2] * t)
-	}
-}
-
-impl Mul<Vec3> for f64 {
-	type Output = Vec3;
-	fn mul(self, vec: Vec3) -> Vec3 {
-		vec * self
-	}
-}
-
-impl Mul<&Vec3> for f64 {
-	type Output = Vec3;
-	fn mul(self, vec: &Vec3) -> Vec3 {
-		vec * self
-	}
-}
-
-impl Mul<Vec3> for u32 {
-	type Output = Vec3;
-	fn mul(self, vec: Vec3) -> Vec3 {
-		vec * self
-	}
-}
-
-impl<T: Into<f64>> Div<T> for Vec3 {
-	type Output = Vec3;
-	fn div(self, t: T) -> Vec3 {
-		self * (1.0 / t.into())
-	}
-}
-
-impl<T: Into<f64>> Div<T> for &Vec3 {
-	type Output = Vec3;
-	fn div(self, t: T) -> Vec3 {
-		self * (1.0 / t.into())
-	}
-}
+impl_op_ex!(+|a: &Vec3, b: &Vec3| -> Vec3 { Vec3::new_with(a[0] + b[0], a[1] + b[1], a[2] + b[2])});
+impl_op_ex!(-|a: &Vec3, b: &Vec3| -> Vec3 {
+	Vec3::new_with(a[0] - b[0], a[1] - b[1], a[2] - b[2])
+});
+impl_op_ex!(-|a: &Vec3| -> Vec3 { Vec3::new_with(-a[0], -a[1], -a[2]) });
+impl_op_ex!(*|a: &Vec3, b: &Vec3| -> Vec3 {
+	Vec3::new_with(a[0] * b[0], a[1] * b[1], a[2] * b[2])
+});
+impl_op_ex_commutative!(*|a: &Vec3, b: f64| -> Vec3 {
+	Vec3::new_with(a[0] * b, a[1] * b, a[2] * b)
+});
+impl_op_ex!(/|a: &Vec3, b: f64| -> Vec3 { a * (1.0 / b) });
 
 fn dot(u: &Vec3, v: &Vec3) -> f64 {
 	u[0] * v[0] + u[1] * v[1] + u[2] * v[2]
@@ -236,33 +131,33 @@ fn ray_color(r: &Ray) -> Color {
 
 fn main() {
 	// Image
-	const aspect_ratio: f64 = 16.0 / 9.0;
-	const image_width: usize = 400;
-	const image_height: usize = ((image_width as f64) / aspect_ratio) as usize;
+	const ASPECT_RATIO: f64 = 16.0 / 9.0;
+	const IMAGE_WIDTH: usize = 400;
+	const IMAGE_HEIGHT: usize = ((IMAGE_WIDTH as f64) / ASPECT_RATIO) as usize;
 
 	// Camera
 	let viewport_height = 2.0;
-	let viewport_width = aspect_ratio * viewport_height;
+	let viewport_width = ASPECT_RATIO * viewport_height;
 	let focal_length = 1.0;
 
 	let origin = Point3::new();
 	let horizontal = Vec3::new_with(viewport_width, 0.0, 0.0);
 	let vertical = Vec3::new_with(0.0, viewport_height, 0.0);
 	let lower_left_corner =
-		&origin - &horizontal / 2 - &vertical / 2 - Vec3::new_with(0.0, 0.0, focal_length);
+		&origin - (&horizontal / 2.0) - (&vertical / 2.0) - Vec3::new_with(0.0, 0.0, focal_length);
 
 	// Render
 
-	let mut pixels = Vec::with_capacity(image_width * image_height);
+	let mut pixels = Vec::with_capacity(IMAGE_WIDTH * IMAGE_HEIGHT);
 
-	for j in (0..image_height).rev() {
+	for j in (0..IMAGE_HEIGHT).rev() {
 		eprint!("\rScanlines remaining: {0}       ", j);
-		for i in 0..image_width {
-			let u = (i as f64) / (image_width as f64 - 1.0);
-			let v = (j as f64) / (image_height as f64 - 1.0);
+		for i in 0..IMAGE_WIDTH {
+			let u = (i as f64) / (IMAGE_WIDTH as f64 - 1.0);
+			let v = (j as f64) / (IMAGE_HEIGHT as f64 - 1.0);
 			let r = Ray::new_with(
 				origin.clone(),
-				&lower_left_corner + u * &horizontal + v * &vertical - &origin,
+				&lower_left_corner + (u * &horizontal) + (v * &vertical) - &origin,
 			);
 
 			let pixel_color = ray_color(&r);
@@ -277,7 +172,7 @@ fn main() {
 	let file = File::create(path).unwrap();
 	let ref mut w = BufWriter::new(file);
 
-	let mut encoder = png::Encoder::new(w, image_width as u32, image_height as u32);
+	let mut encoder = png::Encoder::new(w, IMAGE_WIDTH as u32, IMAGE_HEIGHT as u32);
 	encoder.set_color(png::ColorType::RGB);
 	encoder.set_depth(png::BitDepth::Eight);
 	let mut writer = encoder.write_header().unwrap();
